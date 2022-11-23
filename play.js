8d84cbeea5223f9ce04f9d7626db904c96c16d30
Joe Polo <joe.polo@telus.com>
2022-11-23 12:57:27 -0800

    updated with link to client code and refactored to 3 lines of code

--- play.js
+++ play.js
@@ -1,27 +1,60 @@
-const net = require("net");
+
+//initiates node NET functionality
+// const net = require("net");
+const {connect} = require('./client');
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '192.168.1.64',
-    port: '50541',
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '165.227.47.243',
+//     port: '50541',
+//   });
 
-  conn.setEncoding("utf8");
+//   conn.setEncoding("utf8");
 
-  //on connection, console log a message
-  conn.on("connect", () => {
-    console.log('Snake? Snake!!!')
-  });
-  // interpret incoming data as text
+//   //on connection, console log a message
+//   conn.on("connect", () => {
+//     console.log('Snake? Snake!!!')
+//   });
+//   // interpret incoming data as text
  
-  //on receiving data, console log that data
- conn.on('data', (data) => {
-    console.log(data);
-  });
+//   //on receiving data, console log that data
+//  conn.on('data', (data) => {
+//     console.log(data);
+//   });
 
-  return conn;
-};
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
+
+
+//OLD CODE
+// const net = require("net");
+
+// // establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '165.227.47.243',
+//     port: '50541',
+//   });
+
+//   conn.setEncoding("utf8");
+
+//   //on connection, console log a message
+//   conn.on("connect", () => {
+//     console.log('Snake? Snake!!!')
+//   });
+//   // interpret incoming data as text
+ 
+//   //on receiving data, console log that data
+//  conn.on('data', (data) => {
+//     console.log(data);
+//   });
+
+//   return conn;
+// };
+
+// console.log("Connecting ...");
+// connect();
