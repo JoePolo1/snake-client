30ce979fda7edac725e812b8a736c10f67d8a5c2
Joe Polo <joe.polo@telus.com>
2022-11-23 16:36:14 -0800

    added functionality of taking user input and closing using ctrl c

--- play.js
+++ play.js
@@ -3,32 +3,27 @@
 // const net = require("net");
 const {connect} = require('./client');
 
-// establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: '165.227.47.243',
-//     port: '50541',
-//   });
+const handleUserInput = function (key) {
+  // \u0003 maps to ctrl+c input and closes program if those buttons are pressed
+if (key === '\u0003') {
+  console.log("CTRL + C has been pressed! Exiting Program.")
+  process.exit();
+};
+};
 
-//   conn.setEncoding("utf8");
-
-//   //on connection, console log a message
-//   conn.on("connect", () => {
-//     console.log('Snake? Snake!!!')
-//   });
-//   // interpret incoming data as text
- 
-//   //on receiving data, console log that data
-//  conn.on('data', (data) => {
-//     console.log(data);
-//   });
-
-//   return conn;
-// };
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
 console.log("Connecting ...");
 connect();
-
+setupInput();
 
 //OLD CODE
 // const net = require("net");
